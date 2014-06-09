37fcfd7db4f2bf053196bed17efd95ac8e40e68c
MayRiv <MayRiv@i.ua>
2014-06-09 19:38:50 +0300

    Changed structure, there are 3 projects: view, presenter and models.

--- MayEvolution/MayEvolutionView/Program.cs
+++ MayEvolution/MayEvolutionView/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using MayEvolutionModels;
+using MayEvolutionPresenter;
 using System.Timers;
 namespace MayEvolutionView
 {
